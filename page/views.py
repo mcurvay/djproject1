0790518e9a2f7ccd3aef120521113a1f2c0a15a3
mcurvay <96953565+mcurvay@users.noreply.github.com>
2023-06-11 11:27:27 +0300

    base eklendi

--- page/views.py
+++ page/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "page/homepage.html", {"platform": f"Araba: {randint(1, 10)}"})
+    context=dict()
+    return render(request, "page/homepage.html", context)
 
 def about(request):
     context=dict ()
